68750e201e5dce5d7be177263e3f3595512790d8
vc64 <vchen2022@spyponders.com>
2022-07-10 16:31:57 -0400

    hopefully preserving more quality now

--- overlay.py
+++ overlay.py
@@ -47,7 +47,7 @@ def merge(files, overlay, X, Y):
         file_split = file.split(".")
         # print(file)
         file = "".join(file_split[:-1] + ["Overlayed"] + ["." + file_split[-1]])
-        curr.save(file)
+        curr.save(file, quality=100, subsampling=0)
 
 
 
@@ -99,3 +99,28 @@ elif choice_mode == "2":
     merge(files, overlay, mouseX, mouseY)
 
     cv2.destroyAllWindows()
+
+
+# import sys, getopt
+
+# def main(argv):
+#    inputfile = ''
+#    outputfile = ''
+#    try:
+#       opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+#    except getopt.GetoptError:
+#       print 'test.py -i <inputfile> -o <outputfile>'
+#       sys.exit(2)
+#    for opt, arg in opts:
+#       if opt == '-h':
+#          print 'test.py -i <inputfile> -o <outputfile>'
+#          sys.exit()
+#       elif opt in ("-i", "--ifile"):
+#          inputfile = arg
+#       elif opt in ("-o", "--ofile"):
+#          outputfile = arg
+#    print 'Input file is "', inputfile
+#    print 'Output file is "', outputfile
+
+# if __name__ == "__main__":
+#    main(sys.argv[1:])
