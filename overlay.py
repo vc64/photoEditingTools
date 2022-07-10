fe4644d195ffaa5592da2f37acd4e793bbe3328f
vc64 <vchen2022@spyponders.com>
2022-07-09 23:31:04 -0400

    adding percent input option

--- overlay.py
+++ overlay.py
@@ -27,24 +27,30 @@ elif choice == "2":
 else:
     print("Not a valid option")
 
-# print("Where do you want to place the overlay?")
-# percentX = input("Enter horizontal position as percent (i.e. 0 is left edge, 100 is right edge): ")
-# percentY = input("Enter vertical position as percent (i.e. 0 is top edge, 100 is bottom edge): ")
+print("Where do you want to place the overlay?")
+percentX = input("Enter horizontal position as percent (i.e. 0 is left edge, 100 is right edge): ")
+percentY = input("Enter vertical position as percent (i.e. 0 is top edge, 100 is bottom edge): ")
 
+percentX = int(percentX) / 100
+percentY = int(percentY) / 100
 
 def merge(files, overlay, X, Y):
     origH, origW = Image.open(files[0]).size
+
 
     for file in files:
         curr = Image.open(file)
         currH, currW = curr.size
 
         overH, overW = overlay.size
-        print([Y, currH, origH, overH])
 
-        print((int(Y * currH / origH - (overH / 2)), int(X * currW / origW - (overW / 2))))
+        print((int(Y * currH), int(X * currW)))
 
-        curr.paste(overlay, box = (int(Y * currH / origH - (overH / 2)), int(X * currW / origW - (overW / 2))), mask = overlay)
+        if type(X) == float:
+            print((int(Y * currH), int(X * currW)))
+            curr.paste(overlay, box = (int(X * currW), int(Y * currH)), mask = overlay)
+        else:
+            curr.paste(overlay, box = (int(X * currW / origW), int(Y * currH / origH)), mask = overlay)
 
         file_split = file.split(".")
         # print(file)
@@ -52,44 +58,49 @@ def merge(files, overlay, X, Y):
         curr.save(file)
 
 
-def click_event(event, x, y, flags, params):
-    if event==cv2.EVENT_RBUTTONDOWN:
+merge(files, overlay, percentX, percentY)
+
+
+# def click_event(event, x, y, flags, params):
+#     if event==cv2.EVENT_LBUTTONDOWN:
  
-        print(x, ' ', y)
-        # displaying the coordinates
-        # on the Shell
+#         print(x, ' ', y)
+#         # displaying the coordinates
+#         # on the Shell
         
  
-        # displaying the coordinates
-        # on the image window
-        # font = cv2.FONT_HERSHEY_SIMPLEX
-        # b = img[y, x, 0]
-        # g = img[y, x, 1]
-        # r = img[y, x, 2]
-        # cv2.putText(img, str(b) + ',' +
-        #             str(g) + ',' + str(r),
-        #             (x,y), font, 1,
-        #             (255, 255, 0), 2)
-        # cv2.imshow('image', img)
+#         # # displaying the coordinates
+#         # # on the image window
+#         # font = cv2.FONT_HERSHEY_SIMPLEX
+#         # b = img[y, x, 0]
+#         # g = img[y, x, 1]
+#         # r = img[y, x, 2]
+#         # cv2.putText(img, str(b) + ',' +
+#         #             str(g) + ',' + str(r),
+#         #             (x,y), font, 1,
+#         #             (255, 255, 0), 2)
+#         # cv2.imshow('image', img)
 
 
-        merge(params[0], params[1], x, y)
+#         merge(params[0], params[1], x, y)
 
-        cv2.destroyAllWindows()
+#         cv2.destroyAllWindows()
 
 
-sample = cv2.imread(files[0])
+# sample = cv2.imread(files[0])
 
-crop = [overlay.size[0], (sample.shape[0] - overlay.size[0] + 1), overlay.size[1], (sample.shape[1] - overlay.size[1] + 1)]
+# print(overlay.size, sample.shape)
 
-print(crop)
+# crop = [overlay.size[0]/2, (sample.shape[1] - overlay.size[0]/2 + 1), overlay.size[1]/2, (sample.shape[0] - overlay.size[1]/2 + 1)]
 
-cv2.imshow("image", sample[crop[2]:crop[3], crop[0]:crop[1]])
+# print(crop)
 
-params = [files, overlay]
+# cv2.imshow("image", sample[int(crop[2]):int(crop[3]), int(crop[0]):int(crop[1])])
 
-cv2.setMouseCallback('image', click_event, params)
+# params = [files, overlay]
 
-cv2.waitKey(0)
+# cv2.setMouseCallback('image', click_event, params)
+
+# cv2.waitKey(0)
 
 
