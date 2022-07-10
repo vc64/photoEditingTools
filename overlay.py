79f3ac700147cbe076abeb81c4ebeed9b9af7043
vc64 <vchen2022@spyponders.com>
2022-07-10 17:27:43 -0400

    added command line arguments

--- overlay.py
+++ overlay.py
@@ -17,16 +17,8 @@ print()
 
 overlay = Image.open(filedialog.askopenfilename(title = "Choose overlay image"))
 
-
-choice = input("Image (1) or Folder (2): ")
-
-if choice == "1":
-    files = [filedialog.askopenfilename(title = "Choose base image")]
-elif choice == "2":
-    folderpath = filedialog.askdirectory(title = "Choose folder of base images")
-    files = [folderpath + "/" + x for x in listdir(folderpath)]
-else:
-    print("Not a valid option")
+# img2 = ""
+# img3 = ""
 
 
 def merge(files, overlay, X, Y):
@@ -49,78 +41,94 @@ def merge(files, overlay, X, Y):
         file = "".join(file_split[:-1] + ["Overlayed"] + ["." + file_split[-1]])
         curr.save(file, quality=100, subsampling=0)
 
-
-
-choice_mode = input("Percent (1) or Mouse (2): ")
-
-if choice_mode == "1":
-    print("Where do you want to place the overlay?")
-    percentX = input("Enter horizontal position as percent (i.e. 0 is left edge, 100 is right edge): ")
-    percentY = input("Enter vertical position as percent (i.e. 0 is top edge, 100 is bottom edge): ")
-
-    percentX = int(percentX) / 100
-    percentY = int(percentY) / 100
-
-    merge(files, overlay, percentX, percentY)
-
-elif choice_mode == "2":
-
-    sample = cv2.imread(files[0])
-
-    img2 = sample.copy()
-    img3 = img2.copy()  
-
-    mouseX = 0
-    mouseY = 0
-
-    def click_event(event,x,y,flags,param):
-        global img2,img3, mouseX, mouseY
-
-        if event == cv2.EVENT_MOUSEMOVE:
-            img3 = img2.copy()
-            offsetX = int(param[1].size[0]/2)
-            offsetY = int(param[1].size[1]/2)
-
-            cv2.rectangle(img3,(x-offsetX, y-offsetY),(x+offsetX, y+offsetY),(255,0,0),2)
-        mouseX = x
-        mouseY = y
-
-
-    cv2.namedWindow('IMAGE')
-
-    params = [files, overlay]
-    cv2.setMouseCallback('IMAGE', click_event, params)
-    while 1:
-        cv2.imshow("IMAGE",img3)
-        if cv2.waitKey(20) == ord("c"):
-            print(mouseX, mouseY)
-            break
-
-    merge(files, overlay, mouseX, mouseY)
-
     cv2.destroyAllWindows()
 
+    cv2.imshow("Output", cv2.imread(file))
 
-# import sys, getopt
+    cv2.waitKey(0)
 
-# def main(argv):
-#    inputfile = ''
-#    outputfile = ''
-#    try:
-#       opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-#    except getopt.GetoptError:
-#       print 'test.py -i <inputfile> -o <outputfile>'
-#       sys.exit(2)
-#    for opt, arg in opts:
-#       if opt == '-h':
-#          print 'test.py -i <inputfile> -o <outputfile>'
-#          sys.exit()
-#       elif opt in ("-i", "--ifile"):
-#          inputfile = arg
-#       elif opt in ("-o", "--ofile"):
-#          outputfile = arg
-#    print 'Input file is "', inputfile
-#    print 'Output file is "', outputfile
 
-# if __name__ == "__main__":
-#    main(sys.argv[1:])
+import sys, getopt
+
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv, "hfm", ["folder=", "mouse="])
+    except getopt.GetoptError:
+        print('test.py <arguments> <horizontal percent> <vertical percent>')
+        sys.exit(2)
+    mouse = False
+    folder = False
+    for opt, arg in opts:
+        if opt == '-h':
+            print("test.py <horizontal percent (left = 0)> <vertical percent (top = 0)>")
+            print("\t-h | help")
+            print("\t-f | folder (default = single file)")
+            print("\t-m | cursor select (click c to place)")
+            sys.exit()
+        elif opt in ("-f", "--folder"):
+            folder = True
+        elif opt in ("-m", "--mouse"):
+            mouse = True
+    
+    if folder:
+        folderpath = filedialog.askdirectory(title = "Choose folder of base images")
+        files = [folderpath + "/" + x for x in listdir(folderpath)]
+    else:
+        files = [filedialog.askopenfilename(title = "Choose base image")]
+
+
+    if not mouse:
+        values = []
+        for arg in argv:
+            if not "-" in arg:
+                values.append(int(arg))
+        
+        if len(values) != 2:
+            print("test.py <arguments> <horizontal percent> <vertical percent>")
+            sys.exit(2)
+
+        percentX = int(values[0]) / 100
+        percentY = int(values[1]) / 100
+
+        merge(files, overlay, percentX, percentY)
+    
+    else:
+        sample = cv2.imread(files[0])
+        global img2, img3, mouseX, mouseY
+
+        img2 = sample.copy()
+        img3 = img2.copy()
+
+        mouseX = 0
+        mouseY = 0
+
+        def click_event(event,x,y,flags,param):
+            global img2,img3, mouseX, mouseY
+
+            if event == cv2.EVENT_MOUSEMOVE:
+                img3 = img2.copy()
+                offsetX = int(param[1].size[0]/2)
+                offsetY = int(param[1].size[1]/2)
+
+                cv2.rectangle(img3,(x-offsetX, y-offsetY),(x+offsetX, y+offsetY),(255,0,0),2)
+            mouseX = x
+            mouseY = y
+
+
+        cv2.namedWindow('IMAGE')
+
+        params = [files, overlay]
+        cv2.setMouseCallback('IMAGE', click_event, params)
+        while 1:
+            cv2.imshow("IMAGE",img3)
+            if cv2.waitKey(20) == ord("c"):
+                print(mouseX, mouseY)
+                break
+
+        merge(files, overlay, mouseX, mouseY)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
